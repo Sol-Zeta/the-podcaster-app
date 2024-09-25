fe386d52c9e9404ac9e1d158d0daebc81bb85814
Sol-Zeta <mspattoglio@gmail.com>
2024-09-25 19:55:28 +0200

    Fix Header styles

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import Header from "../Header";
+import Header from "@/components/Header";
 
 describe("Header component", () => {
   test("renders the title and link to the home page", () => {
